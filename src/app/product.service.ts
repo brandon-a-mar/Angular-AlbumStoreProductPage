c43714676207865bffeb6f87dc52ef4da0bd3666
Brandon Mar <44270980+brandon-a-mar@users.noreply.github.com>
2018-10-18 11:32:57 -0700

    Product Interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import { map } from 'rxjs/operators'
+import { Album } from './album'
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -9,9 +11,9 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number) : Observable<Album> {
     return this._http.get(this._albumUrl)
-      .pipe(map(response => response.json()));
+      .pipe(map(response => <Album>response.json()));
   }
 
 }
