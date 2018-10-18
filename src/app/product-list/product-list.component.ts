0d194fb0f7e35a7d2c2e28ad7a654502ecf43ebd
Brandon Mar <44270980+brandon-a-mar@users.noreply.github.com>
2018-10-18 13:04:47 -0700

    one minor change to product list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,10 +11,10 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
 
-  constructor(private productService: ProductService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this.productService.getProducts()
+    this._productService.getProducts()
       .subscribe(response => this.products = response);
   }
 
